8d1d04687705797f8e2c418a94931c36ac472223
franenlanube <ciscosolana@gmail.com>
2023-03-16 08:19:06 +0100

    agregué el archivo correcto a la ramaPruebasLocal

--- modulo3/M3Ex1_Francisco_Solana.java
+++ modulo3/M3Ex1_Francisco_Solana.java
@@ -2,27 +2,29 @@ package modulo3;
 
 import java.util.Scanner;
 
-public class M3Ex1_Francisco_Solana_subido_antes {
+public class M3Ex1_Francisco_Solana {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Variables
-		String nombre;
-		String apellido;
-		int edad;
-		Scanner entrada = new Scanner(System.in);
 		
-		//Lógica
+		// Variables
+		String nombre = "";
+		String apellido = "";
+		int edad = 0;
+		Scanner entrada = new Scanner(System.in);
+
+		// Lógica
 		System.out.println("Diga su nombre");
 		nombre = entrada.nextLine();
+		
 		System.out.println("Diga su apellido");
 		apellido = entrada.nextLine();
+		
 		System.out.println("Diga su edad");
 		edad = entrada.nextInt();
+		
 		entrada.close();
-		
-		
-		//Visualización
+
+		// Visualización
 		System.out.println("Soy " + nombre + " " + apellido + " y tengo " + edad + " años.");
 	}
 
