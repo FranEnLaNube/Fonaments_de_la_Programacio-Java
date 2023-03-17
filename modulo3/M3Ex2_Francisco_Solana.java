11a445d57bf0b24da9220929c3bc6f362374e425
franenlanube <ciscosolana@gmail.com>
2023-03-17 08:44:30 +0100

    Resuelto el conflicto en M3Ex2_Francisco_Solana.java

--- modulo3/M3Ex2_Francisco_Solana.java
+++ modulo3/M3Ex2_Francisco_Solana.java
@@ -2,45 +2,32 @@ package modulo3;
 
 import java.util.Scanner;
 
-public class M3Ex2_Francisco_Solana_Subido_antes {
-=======
 public class M3Ex2_Francisco_Solana {
->>>>>>> ramaPruebas
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//Variables
-<<<<<<< HEAD
-		int num1;
-		int num2;
-=======
+
 		int num1 = 0;
 		int num2 = 0;
->>>>>>> ramaPruebas
+		
+		//Entrada
+		
 		Scanner entrada = new Scanner(System.in);
 
 		//Lógica
+		
 		System.out.println("Ingrese un número");
 		num1 = entrada.nextInt();
 		System.out.println("Ingrese otro número");
 		num2 = entrada.nextInt();
 		entrada.close();
 		
-<<<<<<< HEAD
-
-=======
->>>>>>> ramaPruebas
 		//Visualización
 		System.out.println("la suma da " + (num1 + num2));
 		System.out.println("la resta da " + (num1 - num2));
 		System.out.println("la multiplicación da " + (num1 * num2));
 		System.out.println("la división da " + ((double)num1 / num2));
-<<<<<<< HEAD
-
 	}
-=======
-		}
->>>>>>> ramaPruebas
-
 }
