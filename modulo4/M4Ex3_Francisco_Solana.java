34db8af2e223cd7fe26805e6dc8aa43ac5f5d1f7
franenlanube <ciscosolana@gmail.com>
2023-03-22 13:34:36 +0100

    subo versión final del M4Ex3

--- modulo4/M4Ex3_Francisco_Solana.java
+++ modulo4/M4Ex3_Francisco_Solana.java
@@ -2,7 +2,7 @@ package modulo4;
 
 import java.util.Scanner;
 
-public class M4Ex3_Francisco_SolanaEnClase14nov {
+public class M4Ex3_Francisco_SolanaV2 {
 
 	public static void main(String[] args) {
 
@@ -17,14 +17,6 @@ public class M4Ex3_Francisco_SolanaEnClase14nov {
 		 * l’alumne/a té la beca o no.
 		 */
 
-		/*
-		 * Por el enunciado entiedo que siendo menor de edad puede recibir la beca en
-		 * caso de que este en el paro. Entonces:
-		 */
-
-		// Para entrar en el paro tiene que ser mayor de 16
-		// PERO AL FINAL DIJERON QUE ENTRABA EN EL PARO SI TENÍA 18.
-
 		// Variables
 
 		int edad = 0;
@@ -33,8 +25,6 @@ public class M4Ex3_Francisco_SolanaEnClase14nov {
 		boolean edadBol = true;
 		String tituloTexto = "";
 		String paroTexto;
-		//String respuestaSiNo = ""; AL FINAL ESTE NO LO USÈ
-		//String pudo = ""; // AGREGARON LA VARIABLE enElParo booleana
 
 		Scanner entrada = new Scanner(System.in);
 
@@ -58,27 +48,12 @@ public class M4Ex3_Francisco_SolanaEnClase14nov {
 		tituloBol = (tituloTexto.equalsIgnoreCase("si")) ? true : false;
 		edadBol = (edad >= 18) ? true : false;
 
+		// Salida
+
 		if ((enElParoBol) || (tituloBol && edadBol)) {
 			System.out.println("Tienes la beca.");
 		} else {
 			System.out.println("No Tienes la beca.");
-
-			/*
-			 * if (edad < 16) { pudo = "No"; } else {
-			 * System.out.println("¿Estas en el paro? solo responde \"si\" o \"no\".");
-			 * entrada.nextLine(); respuestaSiNo = entrada.nextLine(); respuestaSiNo =
-			 * respuestaSiNo.toLowerCase(); switch (respuestaSiNo) { case "si": pudo = "Si";
-			 * break; case "no": if (edad >= 18) {
-			 * System.out.println("¿Eres graduado universitario?"); respuestaSiNo =
-			 * entrada.nextLine(); if (respuestaSiNo.equalsIgnoreCase("Si")) { pudo = "Si";
-			 * break; } } pudo = "No"; break; default: System.out.
-			 * println("Solamente puedes insertar \"si\" o \"no\", empieza de vuelta.");
-			 * pudo = "No"; } }
-			 */
-
-			// Salida
-
-			//System.out.println(pudo + " tienes la beca.");
 		}
 	}
 }
