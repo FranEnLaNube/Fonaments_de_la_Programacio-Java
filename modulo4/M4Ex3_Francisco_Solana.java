c27199a323efbc314060bb81ea07868cc740f4aa
franenlanube <ciscosolana@gmail.com>
2023-03-22 13:30:48 +0100

    subo versión con comentarios de clase del M4Ex3

--- modulo4/M4Ex3_Francisco_Solana.java
+++ modulo4/M4Ex3_Francisco_Solana.java
@@ -2,13 +2,12 @@ package modulo4;
 
 import java.util.Scanner;
 
-public class M4Ex3_Francisco_Solana {
+public class M4Ex3_Francisco_SolanaEnClase14nov {
 
 	public static void main(String[] args) {
 
 		/*
-		 * ENUNCIADO. 
-		 * Una escola d’idiomes concedeix beques a futurs estudiants si
+		 * ENUNCIADO. Una escola d’idiomes concedeix beques a futurs estudiants si
 		 * compleixen una sèrie de requisits.
 		 * 
 		 * A l'alumne/a se li assigna una beca si és major d’edat i si té un títol
@@ -24,51 +23,62 @@ public class M4Ex3_Francisco_Solana {
 		 */
 
 		// Para entrar en el paro tiene que ser mayor de 16
+		// PERO AL FINAL DIJERON QUE ENTRABA EN EL PARO SI TENÍA 18.
 
 		// Variables
 
 		int edad = 0;
-		String respuestaSiNo = "";
-		String pudo = "";
+		boolean tituloBol;
+		boolean enElParoBol;
+		boolean edadBol = true;
+		String tituloTexto = "";
+		String paroTexto;
+		//String respuestaSiNo = ""; AL FINAL ESTE NO LO USÈ
+		//String pudo = ""; // AGREGARON LA VARIABLE enElParo booleana
 
 		Scanner entrada = new Scanner(System.in);
 
 		// Logica
 
+		// Solicitud den información
+		// 1ra variable
+		System.out.println("¿Estás en el paro?");
+		paroTexto = entrada.next();
+		// 2da Variable
 		System.out.println("¿Qué edad tienes?");
-
 		edad = entrada.nextInt();
-		if (edad < 16) {
-			pudo = "No";
-		} else {
-			System.out.println("¿Estas en el paro? solo responde \"si\" o \"no\".");
-			entrada.nextLine();
-			respuestaSiNo = entrada.nextLine();
-			respuestaSiNo = respuestaSiNo.toLowerCase();
-			switch (respuestaSiNo) {
-			case "si":
-				pudo = "Si";
-				break;
-			case "no":
-				if (edad >= 18) {
-					System.out.println("¿Eres graduado universitario?");
-					respuestaSiNo = entrada.nextLine();
-					if (respuestaSiNo.equalsIgnoreCase("Si")) {
-						pudo = "Si";
-						break;
-					}
-				}
-				pudo = "No";
-				break;
-			default:
-				System.out.println("Solamente puedes insertar \"si\" o \"no\", empieza de vuelta.");
-				pudo = "No";
-			}
-		}
-
-		// Salida
-
-		System.out.println(pudo + " tienes la beca.");
+		System.out.println("¿Eres graduado universitario?");
+		// edad = entrada.nextInt();
+		tituloTexto = entrada.next();
 		entrada.close();
+
+		// Conversiones
+
+		enElParoBol = (paroTexto.equalsIgnoreCase("si")) ? true : false;
+		tituloBol = (tituloTexto.equalsIgnoreCase("si")) ? true : false;
+		edadBol = (edad >= 18) ? true : false;
+
+		if ((enElParoBol) || (tituloBol && edadBol)) {
+			System.out.println("Tienes la beca.");
+		} else {
+			System.out.println("No Tienes la beca.");
+
+			/*
+			 * if (edad < 16) { pudo = "No"; } else {
+			 * System.out.println("¿Estas en el paro? solo responde \"si\" o \"no\".");
+			 * entrada.nextLine(); respuestaSiNo = entrada.nextLine(); respuestaSiNo =
+			 * respuestaSiNo.toLowerCase(); switch (respuestaSiNo) { case "si": pudo = "Si";
+			 * break; case "no": if (edad >= 18) {
+			 * System.out.println("¿Eres graduado universitario?"); respuestaSiNo =
+			 * entrada.nextLine(); if (respuestaSiNo.equalsIgnoreCase("Si")) { pudo = "Si";
+			 * break; } } pudo = "No"; break; default: System.out.
+			 * println("Solamente puedes insertar \"si\" o \"no\", empieza de vuelta.");
+			 * pudo = "No"; } }
+			 */
+
+			// Salida
+
+			//System.out.println(pudo + " tienes la beca.");
+		}
 	}
 }
