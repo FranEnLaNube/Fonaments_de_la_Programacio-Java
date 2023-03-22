5bfb86ec97eefb8586bb3d2aeee6f907345fdbb6
franenlanube <ciscosolana@gmail.com>
2023-03-22 15:18:50 +0100

    subo M4Ex5, la versión de Juan Andrés

--- modulo4/M4Ex5_Francisco_Solana.java
+++ modulo4/M4Ex5_Francisco_Solana.java
@@ -2,68 +2,66 @@ package modulo4;
 
 import java.util.Scanner;
 
-public class M4Ex5_Francisco_SolanaEnClase14nov {
+public class M4Ex5_Juan_Andres {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		/*
-		 * Fer un programa que demani dos números i un operador(+,-,*,/).
-		 * 
-		 * Al final, el programa ha d'imprimir per pantalla el resultat de fer
-		 * l’operació que contingui la variable operador.
-		 */
+		// Demanar a l'usuari que introdueixi dos números i un operador
+		// Representar a pantalla el resultat de l'operació
 
 		int num1 = 0;
 		int num2 = 0;
-		//String operador = "";
+		// String operador = "";
 		char operadorChar = ' ';
 		Scanner entrada = new Scanner(System.in);
 
-		// Solicito al usuario
-
+		// Sol·licitud de dades
 		System.out.println("Indica un valor inicial");
 		num1 = entrada.nextInt();
-		System.out.println("Indica un valor para el segundo numero");
+		System.out.println("Indica un valor pel segon número");
 		num2 = entrada.nextInt();
-		System.out.println("¿Qué operación deseas realizar (+, -, *, /)");
-		// operador = entrada.next();
-		// HAREMOS UN METODO PROPIO DEL STRING PARA CREAR VARIABLE CHAR
-		operadorChar = entrada.next().charAt(0); // Conversión de String a char.
-		// los caracteres se numeran del 0 en adelante, por el orden de la palabra, pe.
-		// para carlos, c es 0.
-		// Los espacios en blanco también cuentan.
+		System.out.println("Quina operació desitges realitzar? (+,-,*,/)");
+		// operador = entrada.next(); // versió 1
+		operadorChar = entrada.next().charAt(0); // conversió d'string a char
 		entrada.close();
 
-		// Logica
+		// Oriol
+		// O = index 0
+		// r = index 1
+		// (...)
+
+		// Construcció de la lògica
 		/*
-		 * VERSION 1 if(operador.equals("+")) {
-		 * System.out.println("El resultado de la suma es " + (num1 + num2));
-		 * 
-		 * } else if(operador.equals("-")) {
-		 * System.out.println("El resultado de la resta es " + (num1 - num2)); } else
-		 * if(operador.equals("*")) {
-		 * System.out.println("El resultado de la multiplicación es " + (num1 * num2));
-		 * } else if(operador.equals("/")) {
-		 * System.out.println("El resultado de la división es " + (num1 / num2)); } else
-		 * { System.out.println("Solo puedes escribir +, -, * o /");
+		 * - VERSIÓ 1 if( operador.equals("+")){
+		 * System.out.println("El resultat de la SUMA és " + (num1 + num2)); } else
+		 * if(operador.equals("-")){ System.out.println("El resultat de la RESTA és "+
+		 * (num1 - num2)); } else if(operador.equals("*")) {
+		 * System.out.println("El resultat de la MULTIPLICACIÓ és "+ (num1 * num2)); }
+		 * else if(operador.equals("/")) {
+		 * System.out.println("El resultat de la DIVISIÓ és "+ (float)(num1 / num2)); }
+		 * else { System.out.println("Introdueix un operador correcte (+,-,*,/)"); }
 		 */
-		//VERSION 2
+
 		switch (operadorChar) {
 		case '+':
-			System.out.println("El resultado de la suma es " + (num1 + num2));
+			System.out.println("El resultat de la SUMA és " + (num1 + num2));
 			break;
 		case '-':
-			System.out.println("El resultado de la resta es " + (num1 - num2));
+			System.out.println("El resultat de la RESTA és " + (num1 - num2));
 			break;
 		case '*':
-			System.out.println("El resultado de la multiplicación es " + (num1 * num2));
+			System.out.println("El resultat de la MULTIPLICACIÓ és " + (num1 * num2));
 			break;
 		case '/':
-			System.out.println("El resultado de la división es " + (num1 / num2));
+			System.out.println("El resultat de la DIVISIÓ és " + (float) (num1 / num2));
 			break;
 		default:
-			System.out.println("Ingresa un operador correcto (+, -, * o /4)");
+			System.out.println("Introdueix un operador correcte (+,-,*,/)");
+			break;
 
 		}
+
 	}
+
 }
