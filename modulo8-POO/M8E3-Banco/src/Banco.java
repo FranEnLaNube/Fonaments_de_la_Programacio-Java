39b9407b7df33c343a336eff69c77595452f6d56
franenlanube <ciscosolana@gmail.com>
2023-06-18 13:07:44 +0200

    Preliminary version

--- modulo8-POO/M8E3-Banco/src/Banco.java
+++ modulo8-POO/M8E3-Banco/src/Banco.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
+//Corregir bug de que te permite acceder al mismo numero de cuenta pero con nombres de clientes distintos
 public class Banco {
-
+    private static ArrayList<Cliente> cliente = new ArrayList<Cliente>();
     private static Scanner input = new Scanner(System.in);
     private static String nombre = "";
 	private static String apellido = "";
@@ -22,8 +23,14 @@ public class Banco {
             input.nextLine();
             switch (option) {
 			case 0:
-                System.out.println("hasta luego");
+                System.out.println("Hasta luego");
 				end = true;
+				break;
+            case 1:
+				pedirDatos();
+				Cliente cl = new Cliente(nombre, apellido);
+				cliente.add(cl);
+				System.out.println("El cliente ha sido agregado");
 				break;
 			case 2:
 				pedirDatos();
@@ -35,6 +42,26 @@ public class Banco {
 					System.out.println("El cliente ha sido eliminado");
 				}
 				break;
+            case 3:
+				pedirDatos();
+				int idCliente = findIndex(nombre, apellido);
+				if (idCliente < 0) {
+					System.out.println("El cliente no existe");
+				} else {
+					Cuenta cuenta1 = new Cuenta(CreadorID.nextVal(), 0);
+					cliente.get(idCliente).addCuenta(cuenta1);
+					System.out.println("Se creo la cuenta " + cuenta1.getNroCuenta() 
+					+ " para el cliente " + nombre + " " + apellido + ".");
+                }
+                break;
+            case 4:
+				modificarSaldo(true);
+				break;
+            case 5:
+				modificarSaldo(false);
+				break;
+            default:
+				System.out.println("Digite una opcion correcta");
             }
         } while (end == false);
     }
@@ -54,5 +81,30 @@ public class Banco {
 			}
 		}
 		return -1;
+	}
+    public static void modificarSaldo(boolean suma) {
+		pedirDatos();
+		int iClient = findIndex(nombre, apellido);
+		if (iClient < 0) {
+			System.out.println("El cliente no existe");
+		} else {
+			System.out.println("Ingrese número de cuenta");
+			int nc = input.nextInt();
+			input.nextLine();
+			Cliente clienteEncontrado = cliente.get(iClient);
+			int j = clienteEncontrado.findIndexCuenta(nc);
+			if (j < 0) {
+				System.out.println("La cuenta no existe");
+			} else {
+				System.out.println("Cuál es el monto?");
+				int deposito = input.nextInt();
+				input.nextLine();
+				if (!suma) {
+					deposito *= -1;
+				}
+				int saldo = clienteEncontrado.getCuentas().get(j).opSaldo(deposito);
+				System.out.println("El saldo actual es " + saldo);
+			}
+		}
 	}
 }
