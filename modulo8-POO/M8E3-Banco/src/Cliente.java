b400b1f30e69ea478aad40e7efd3c8dfbe81bd44
franenlanube <ciscosolana@gmail.com>
2023-06-18 11:05:45 +0200

    Initial commit for Banco and Client

---
+++ modulo8-POO/M8E3-Banco/src/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+    // Atributos
+    private String nombre;
+    private String apellido;
+    //Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
